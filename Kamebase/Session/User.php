8ee9795ab75451d653822f185ee94d2707d6835d
HAlex <halexercraft@gmail.com>
2018-03-14 19:04:36 +0100

    Add a User entity that can be used in the future

---
+++ Kamebase/Session/User.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by HAlexTM on 14/03/2018 14:31
+ */
+
+namespace Kamebase\Session;
+
+
+use Kamebase\Entity\Entity;
+
+/**
+ * @property int id
+ */
+class User extends Entity {
+
+}
